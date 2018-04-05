6c6dd4733ac724fae29241c1ae16bd69e8a76f69
Maxime <maximembc@hotmail.fr>
2018-04-05 18:14:37 +0200

    add api

--- MySuperHeroGame/src/app/app.component.ts
+++ MySuperHeroGame/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient){
+  }
+  ngOnInit(): void {
+    this.http.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
