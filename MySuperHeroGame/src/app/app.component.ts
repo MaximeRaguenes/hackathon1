65458aaae45b8ab63ed27f3a8975d688cd27ac59
Maxime <maximembc@hotmail.fr>
2018-04-05 23:08:47 +0200

    z

--- MySuperHeroGame/src/app/app.component.ts
+++ MySuperHeroGame/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'My Heroes game';
   heroes;
   herop1;
