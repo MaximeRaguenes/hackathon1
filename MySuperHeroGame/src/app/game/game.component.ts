524aa4fc65ad8b4823dedbf41b95769254510f64
Maxime <maximembc@hotmail.fr>
2018-04-06 06:39:44 +0200

    add magie healing

--- MySuperHeroGame/src/app/game/game.component.ts
+++ MySuperHeroGame/src/app/game/game.component.ts
@@ -28,10 +28,40 @@ export class GameComponent implements OnInit {
   get herop2() {
     return this.playersService.herop2;
   }
-  attack(atq) {
-      this.hpPlayer2 = this.hpPlayer2 - this.playersService.herop1.powerstats.combat;
-      if (this.hpPlayer2 <= 0) {
-        alert(' Winner is :  {{ playersService.herop1.name }}' );
-      }
+
+  attackP1(atq) {
+    this.hpPlayer2 = this.hpPlayer2 - this.playersService.herop1.powerstats.combat;
+    if (this.hpPlayer2 <= 0) {
+      this.hpPlayer2 = 0;
+      alert(' Winner is ' + this.herop1.name);
     }
+  }
+  magieP1(mage) {
+    this.hpPlayer2 = this.hpPlayer2 - this.playersService.herop1.powerstats.intelligence;
+    if (this.hpPlayer2 <= 0) {
+      this.hpPlayer2 = 0;
+      alert(' Winner is ' + this.herop1.name);
+    }
+  }
+  healingP1(heal) {
+    this.hpPlayer1 = this.hpPlayer1 + 25;
+  }
+
+  attackP2(atq) {
+    this.hpPlayer1 = this.hpPlayer1 - this.playersService.herop2.powerstats.combat;
+    if (this.hpPlayer1 <= 0) {
+      this.hpPlayer1 = 0;
+      alert(' Winner is ' + this.herop2.name);
+    }
+  }
+  magieP2(mage) {
+    this.hpPlayer1 = this.hpPlayer1 - this.playersService.herop2.powerstats.intelligence;
+    if (this.hpPlayer1 <= 0) {
+      this.hpPlayer1 = 0;
+      alert(' Winner is ' + this.herop2.name);
+    }
+  }
+  healingP2(heal) {
+    this.hpPlayer2 = this.hpPlayer2 + 25;
+  }
 }
